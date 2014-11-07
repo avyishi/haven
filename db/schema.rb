562d1a3a837a09bd9a33d432d72a07c37ab62b52
avyishi <avyishi@gmail.com>
2014-11-07 00:28:15 -0800

    Updated movies page and users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141106031332) do
+ActiveRecord::Schema.define(version: 20141106084409) do
 
   create_table "movies", force: true do |t|
     t.datetime "created_at"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 20141106031332) do
     t.string   "unconfirmed_email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
