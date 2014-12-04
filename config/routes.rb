323b766587f350d0a21626ffbce74967f22dae4c
avyishi <avyishi@gmail.com>
2014-12-04 00:52:51 -0800

    More updates to text

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :movies
-    resources :reviews
-  
-  resources :shows
+  resources :movies do
+    resources :reviews, except: [:index]
+  end
+  resources :shows  do
+    resources :reviews, except: [:index]
+  end
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
