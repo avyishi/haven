2d83854ab91006f985718eee669ddc580fcb8d45
avyishi <avyishi@gmail.com>
2015-01-17 12:54:57 -0800

    Updated about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-
+  
   resources :movies do
     collection do
       get 'search'
